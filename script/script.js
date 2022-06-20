65d0404c5dc764320a2a9c447a1af0e5bf6f268b
brevanmeter <brevuelta.recover2020@gmail.com>
2022-06-20 12:57:10 -0600

    fixing second review

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close-icon");
 let popup = document.querySelector(".popup");
 let insertName = document.querySelector(".popup__insert-name");
+let insertAbout = document.querySelector(".popup__insert-about");
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let popupSaveButton = document.querySelector(".popup__save-button");
@@ -10,7 +11,7 @@ let cardLikeButton = document.querySelector(".card__like-button");
 function editPopup() {
     popup.classList.toggle("popup_opened");
     insertName.value = profileName.textContent;
-    insertName.value = profileDescription.textContent; 
+    insertAbout.value = profileDescription.textContent; 
 }
 
 function savePopup(evt) {
