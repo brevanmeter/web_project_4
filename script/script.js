40c4a139761a4f95d2c4061bb62f8e2c4a425927
brevanmeter <brevuelta.recover2020@gmail.com>
2022-07-04 15:03:28 -0600

    working on project 5

--- script/script.js
+++ script/script.js
@@ -1,33 +1,130 @@
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close-icon");
-let popup = document.querySelector(".popup");
-let insertName = document.querySelector(".popup__insert-name");
-let insertAbout = document.querySelector(".popup__insert-about");
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
-let popupSaveButton = document.querySelector(".popup__save-button");
-let cardLikeButton = document.querySelector(".card__like-button");
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closePopupIcon = document.querySelectorAll(".popup__close-icon");
+const popupEditProfile = document.querySelector(".popup__edit-profile");
+const insertName = document.querySelector(".popup__insert-name");
+const insertAbout = document.querySelector(".popup__insert-about");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const saveButtonAddCard = document.querySelector(".popup__save-button-addcard");
+const cardTemplate = document.querySelector(".card__template").content;
+const elements = document.querySelector(".elements");
+const addButton = document.querySelector(".profile__add-button");
+const popupAddCard = document.querySelector(".popup__add-card");
+const insertTitle = document.querySelector(".popup__insert-title");
+const insertLink = document.querySelector(".popup__insert-link");
+const popupImage = document.querySelector(".popup__image");
+const popupSaveButton = document.querySelector(".popup__save-button");
+const addCardTitle = document.querySelector(".popup__title");
+const addCardLink = document.querySelector(".popup__link");
+
+//1. GERA OS 6 CARDS INICIAIS
+
+const initialCards = [
+    {
+      name: "Vale de Yosemite",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    },
+    {
+      name: "Lago Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    },
+    {
+      name: "Montanhas Carecas",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    },
+    {
+      name: "Parque Nacional da Vanoise ",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+  ];
+  
+  
+  function callInitialCards() {
+    elements.innerHTML = "";
+    initialCards.map(function (item) {
+      const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+      cardElement.querySelector(".card__text").textContent = item.name;
+      cardElement.querySelector(".card__image").src = item.link;
+      cardElement.querySelector(".card__image").alt = item.name;
+      return elements.append(cardElement);
+    });
+    callLikeButton();
+    callPopupImage();
+    deletCardButton();
+  }
+
+//2. ABERTURA DE POPUPS 
+
+//2.1 POPUP EDIT PROFILE / SAVE POPUP EDIT PROFILE INFO
 
 function editPopup() {
-    popup.classList.toggle("popup_opened");
+    popupEditProfile.classList.toggle("popup_opened");
     insertName.value = profileName.textContent;
-    insertAbout.value = profileDescription.textContent; 
+    insertAbout.value = profileDescription.textContent;
 }
 
 function savePopup(evt) {
     profileName.textContent = insertName.value;
     profileDescription.textContent = insertAbout.value;
-    popup.classList.toggle("popup_opened");
+    popupEditProfile.classList.toggle("popup_opened");
     evt.preventDefault();
 }
 
-/*
-for (let i = 0; i < cardLikeButton.length; i++) {
-    cardLikeButton[i].addEventListener("click", callCardLikeButton);   
-}
-function callCardLikeButton() {
-}*/
-
 editButton.addEventListener("click", editPopup);
-closeButton.addEventListener("click", editPopup);
-popupSaveButton.addEventListener("click", savePopup); 
+popupSaveButton.addEventListener("click", savePopup);
+
+//2.2 POPUP ADD CARD / SAVE POPUP ADD CARD INFO
+
+function callPopupAddCard() {
+    popupAddCard.classList.toggle("popup_opened");
+}
+   
+addButton.addEventListener("click", callPopupAddCard);
+
+
+//2.3 POPUP IMAGE
+
+function callPopupImage(evt) {
+        popupImage.classList.toggle("popup_opened");
+        popupImage.src = evt.target.src;
+    }
+    
+    elements.addEventListener("click", callPopupImage); 
+
+function createPopupImage(evt) {
+    const imageView = document.querySelector(".popup__image-view");
+    imageView.src = evt.target.src;
+    imageView.alt = evt.target.alt;
+    const imageTitle = document.querySelector(".popup__caption");
+    imageTitle.textContent = evt.target.alt;
+    popupImage.classList.toggle("popup_opened");
+}  
+
+//3 FECHA OS POPUPS
+
+closePopupIcon.forEach(function (item) {
+    item.addEventListener("click", function () {
+      closePopup(item.closest(".popup"));
+    });
+  });
+  
+  function closePopup(item) {
+    item.classList.remove("popup_opened");
+  }
+
+callInitialCards();
+
+
+
+
+
+
